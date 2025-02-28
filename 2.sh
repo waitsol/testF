424e21edb7c3bbb41dee7b1b02d184a78363e1fb
zq <zq@example.com>
2025-02-28 22:02:38 +0800

    /root/xxx/aa/testF/2025-02-28_22-02-32

---
+++ 2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set +e
+GIT_BRANCH=main
+GIT_SERVER_RES=qq/xx/
+p4rev=$(cat $LOG_PATH/.p4rev)
+
+# 获取当前路径（工作目录）
+current_path=$(pwd)
+GIT_SERVER_SRC=${current_path}
+
+# 获取当前时间（自定义格式，示例：2024-03-15_12-30-45）
+current_time=$(date "+%Y-%m-%d_%H-%M-%S")
+
+# 拼接路径和时间
+COMMIT_LOG="${current_path}/${current_time}"
+
+cd -P $GIT_SERVER_RES
+
+git pull origin $GIT_BRANCH --rebase
+
+
+GIT_RES_COMMIT=$(git rev-parse HEAD)
+echo "GIT_RES_COMMIT=${GIT_RES_COMMIT}"
+
+cd $GIT_SERVER_SRC
+
+git pull origin $GIT_BRANCH --rebase
